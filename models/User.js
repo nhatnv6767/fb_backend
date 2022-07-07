2876fdfd9b3d3639c60e3326771bf17a2ea78ab6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-07 22:20:33 +0900

    #20 User modal 3

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     first_name: {
         type: String,
-        required: true,
-    }
+        required: [true, "first_name is required"],
+        // if have any spaces, it's going to remove them
+        trim: true,
+        // use it to search
+        text: true,
+    },
 });
