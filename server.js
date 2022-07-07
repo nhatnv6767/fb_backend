65a4693b96d24aa9aff4714933563b6a72cd93be
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-07 20:40:18 +0900

    #10 Cors 3

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const options = {
+  origin: "http://localhost:3000"
+}
 
-app.use(cors());
+app.use(cors(options));
 
 app.get("/", (req, res) => {
   res.send("welcome from home");
