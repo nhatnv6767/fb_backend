15b734d1a3b4a896253f5ff0d95d9519b5acca5d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-07 23:00:22 +0900

    #22 Register and validate data using regex 5

--- controllers/user.js
+++ controllers/user.js
@@ -13,16 +13,6 @@ exports.register = async (req, res) => {
         gender,
     } = req.body;
 
-    const user = await new User({
-        first_name,
-        last_name,
-        email,
-        password,
-        username,
-        bYear,
-        bMonth,
-        bDay,
-        gender,
-    }).save();
+    const user = await new User(req.body).save();
     res.json(user);
 };
