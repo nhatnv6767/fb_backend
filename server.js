17c795f64c1ae65d8824af66fc45ca7dbc0e9895
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-07 21:00:11 +0900

    #10 Cors 6

--- server.js
+++ server.js
@@ -2,24 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-let allowed=["http://localhost:3000", "some another link"]
-let options = (req, res) => {
-  let tmp;
-  let origin = req.header("Origin");
-  if(allowed.indexOf(origin) > -1) {
-    tmp = {
-      origin: true,
-      optionSuccessStatus: 200,
-    }
-  } else {
-    tmp = {
-      origin: "nothing",
-    }
-  }
-  res(null, tmp);
-}
-
-app.use(cors(options));
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("welcome from home");
