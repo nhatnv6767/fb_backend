7146344ccb0e1567e40a7b7bfb745f0a93a3b47d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 14:06:25 +0900

    #61 Authentification middleware 5

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,14 @@ const {
     login,
     auth,
 } = require("../controllers/user");
+const {authUser} = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/activate", activateAccount);
 router.post("/login", login);
-router.post("/auth", auth);
+/* A middleware that checks if the user is authenticated. */
+router.post("/auth", authUser, auth);
 
 module.exports = router;
