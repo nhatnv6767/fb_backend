e31da2b0a8e72b8826d6d16328497201ecd3559f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 03:04:15 +0900

    #71 Find User 1 -> Add API in backend 3

--- controllers/user.js
+++ controllers/user.js
@@ -172,7 +172,15 @@ exports.sendVerification = async (req, res) => {
 exports.findUser = async (req, res) => {
     try {
         const {email} = req.body;
-        const user = await User.findOne({email});
+        const user = await User.findOne({email}).select("-password");
+        if (!user) {
+            return res.status(400).json({message: "Account does not exists."});
+        }
+
+        return res.status(200).json({
+            email: user.email,
+            picture: user.picture,
+        })
     } catch (e) {
         res.status(500).json({message: e.message});
     }
