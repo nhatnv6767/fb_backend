571abcf73b64bf5a9052578e3ea2b23efa8a5e61
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 14:07:53 +0900

    #61 Authentification middleware 6

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 exports.authUser = async (req, res, next) => {
     try {
         let tmp = req.header("Authorization");
-        const token = tmp.slice(7, tmp.length);
+        const token = tmp ? tmp.slice(7, tmp.length) : "";
         if (!token) {
             return res.status(400).json({message: "Invalid Authentification"});
         }
