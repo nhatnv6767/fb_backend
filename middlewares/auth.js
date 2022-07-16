7ca8dfd564608bc2e8fa30248d4b5b32d07ad7da
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-16 14:04:36 +0900

    #61 Authentification middleware 4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,8 +8,16 @@ exports.authUser = async (req, res, next) => {
             return res.status(400).json({message: "Invalid Authentification"});
         }
         jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-
-        })
+            if (err) {
+                return res.status(400).json({message: "Invalid Authentification"});
+            }
+            /* Setting the user object to the request object. */
+            req.user = user;
+            /* `next()` is a function that is passed to the middleware function.
+            It is used to call the next middleware
+            function in the stack. */
+            next();
+        });
     } catch (e) {
         return res.status(500).json({message: e.message});
     }
