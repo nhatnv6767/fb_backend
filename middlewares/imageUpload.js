1ce8c2135ebaceba9d5ecc05f86d1bad4ffacf14
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 23:56:47 +0900

    #82 Upload images to cloudinary 8

--- middlewares/imageUpload.js
+++ middlewares/imageUpload.js
@@ -1,6 +1,14 @@
 module.exports = async function (req, res, next) {
     try {
-        console.log(Object.values(req.files).flat());
+        if (!req.files || Object.values(req.files).flat().length === 0) {
+            return res.status(400).json({message: "No files selected."});
+        }
+        let files = Object.values(req.files).flat();
+        files.forEach((file) => {
+            if (file.mimetype !== "image/jpeg") {
+                return res.status(400).json({message: "Unsupported format."});
+            }
+        });
     } catch (e) {
         res.status(500).json({message: e.message});
     }
