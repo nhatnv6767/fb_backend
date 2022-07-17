8c8264ecac39b3607f6b159800e88230f4219382
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-18 00:30:29 +0900

    #83 <2> Upload images to cloudinary 3

--- controllers/upload.js
+++ controllers/upload.js
@@ -8,7 +8,18 @@ cloudinary.config({
 exports.uploadImages = async (req, res) => {
     try {
         const {path} = req.body;
+        let files = Object.values(req.files).flat();
+        let images = [];
+        for (const file of files) {
+            const url = await uploadToCloudinary(file, path);
+            images.push(url);
+        }
+        res.json(images);
     } catch (e) {
         res.status(500).json({message: e.message});
     }
 };
+
+const uploadToCloudinary = async (file, path) => {
+
+}
