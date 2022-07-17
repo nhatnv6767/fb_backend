5582674ee77009f102449fd10414967860fbab98
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 11:29:32 +0900

    #72 Send reset code email 1

---
+++ models/Code.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const {ObjectId} = mongoose.Schema;
+
+const codeSchema = new mongoose.Schema({
+    code: {
+        type: Number,
+        required: true,
+    },
+    user: {
+        type: ObjectId,
+        ref: "User",
+        required: true,
+    }
+});
