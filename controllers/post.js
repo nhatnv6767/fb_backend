15b5b51ecc28500857130adbbd67cc83d9571329
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 23:16:45 +0900

    #81 Post model & Create post func 5

--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,8 @@
 exports.createPost = async (req, res) => {
-
-}
+    try {
+        const post = await new Post(req.body).save();
+        res.json(post);
+    } catch (e) {
+        res.status(500).json({message: e.message});
+    }
+};
