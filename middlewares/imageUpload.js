eca6c9acc73436140485b746aba3513818af3f90
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 23:43:29 +0900

    #82 Upload images to cloudinary 7 => Array

--- middlewares/imageUpload.js
+++ middlewares/imageUpload.js
@@ -1,6 +1,6 @@
 module.exports = async function (req, res, next) {
     try {
-        console.log((req.files));
+        console.log(Object.values(req.files));
     } catch (e) {
         res.status(500).json({message: e.message});
     }
