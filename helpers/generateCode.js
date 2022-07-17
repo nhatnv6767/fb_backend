e23d5d397c3b677cb709035633add6cd4465310e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 12:20:37 +0900

    #72 Send reset code email 6

---
+++ helpers/generateCode.js
@@ -0,0 +1,9 @@
+export default function generateCode(length) {
+    let code = "";
+    let schema = "0123456789";
+
+    for (let i = 0; i < length; i++) {
+        /* Generating a random number between 0 and the length of the schema string. */
+        code += schema.charAt(Math.floor(Math.random() * schema.length));
+    }
+}
