784f4b9b7af6d1d4677994ae4e4ed910391d54b0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-18 00:26:39 +0900

    #83 <2> Upload images to cloudinary 2

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,6 +1,13 @@
+const cloudinary = require('cloudinary');
+const fs = require('fs');
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+});
 exports.uploadImages = async (req, res) => {
     try {
-
+        const {path} = req.body;
     } catch (e) {
         res.status(500).json({message: e.message});
     }
