2512ba41ac3214495d2f2ea577a67e4b4fbe38c2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-17 23:35:42 +0900

    #82 Upload images to cloudinary 6

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(fileUpload({
+    /* Used to store the file temporarily in the server. */
     useTempFiles: true,
 }));
 
