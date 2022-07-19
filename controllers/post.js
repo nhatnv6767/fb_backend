5ae01d304d88289c65eb3121cc2d9767e4cd8c7c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-19 21:48:47 +0900

    #91 Post component-header 6

--- controllers/post.js
+++ controllers/post.js
@@ -10,7 +10,9 @@ exports.createPost = async (req, res) => {
 
 exports.getAllPosts = async (req, res) => {
     try {
-        const posts = await Post.find().populate("user");
+        const posts = await Post.find().populate("user",
+            "first_name last_name picture username gender"
+        );
         res.json(posts);
     } catch (e) {
         res.status(500).json({message: e.message});
