cb213312d4d952bcf8c22227fa891d3f5901cc12
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-19 22:04:03 +0900

    #91 Post component-header 17

--- controllers/post.js
+++ controllers/post.js
@@ -12,7 +12,8 @@ exports.getAllPosts = async (req, res) => {
     try {
         const posts = await Post.find().populate("user",
             "first_name last_name picture username gender"
-        );
+            /* Sorting the posts by the date they were created. */
+        ).sort({createdAt: "desc"});
         res.json(posts);
     } catch (e) {
         res.status(500).json({message: e.message});
