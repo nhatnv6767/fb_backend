b41c5470252231b699845df6adcea0dacde5d779
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-19 22:04:30 +0900

    #91 Post component-header 17 same

--- controllers/post.js
+++ controllers/post.js
@@ -13,7 +13,7 @@ exports.getAllPosts = async (req, res) => {
         const posts = await Post.find().populate("user",
             "first_name last_name picture username gender"
             /* Sorting the posts by the date they were created. */
-        ).sort({createdAt: "desc"});
+        ).sort({createdAt: -1});
         res.json(posts);
     } catch (e) {
         res.status(500).json({message: e.message});
