41d726302d49726d0067a386e588420ab9d9342a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-02 23:11:36 +0900

    #132 React mode, react function 8

--- controllers/react.js
+++ controllers/react.js
@@ -5,8 +5,17 @@ exports.reactPost = async (req, res) => {
         const {postId, react} = req.body;
         const check = await React.findOne({
             postRef: postId,
-            reactBy: req.user.id,
+            reactBy: mongoose.Types.ObjectId(req.user.id),
         });
+
+        if (check == null) {
+            const newReact = new React({
+                react: react,
+                postRef: postId,
+                reactBy: req.user.id,
+            });
+            await newReact.save();
+        }
     } catch (e) {
         res.status(500).json({message: e.message});
     }
