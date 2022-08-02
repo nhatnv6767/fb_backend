ad109fd75167e4f3cf688bd0fdf8a468a4e5f6aa
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-02 23:07:10 +0900

    #132 React mode, react function 6

--- controllers/react.js
+++ controllers/react.js
@@ -2,7 +2,10 @@ const React = require("../models/React");
 exports.reactPost = async (req, res) => {
     try {
         const {postId, react} = req.body;
-        const check = await React.findOne({});
+        const check = await React.findOne({
+            postRef: postId,
+            reactBy: req.user.id,
+        });
     } catch (e) {
         res.status(500).json({message: e.message});
     }
