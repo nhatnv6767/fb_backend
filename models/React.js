9b22a63d4d93d9419993e2c70ab66d70bcd5a7be
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-02 22:58:42 +0900

    #132 React mode, react function 2

--- models/React.js
+++ models/React.js
@@ -3,6 +3,20 @@ const mongoose = require('mongoose');
 /* Destructuring the `ObjectId` property from the `mongoose.Schema` object. */
 const {ObjectId} = mongoose.Schema;
 
-const reactSchema = new mongoose.Schema({});
+const reactSchema = new mongoose.Schema({
+    react: {
+        type: String,
+        enum: ['like', 'love', 'haha', 'sad', 'angry', 'wow'],
+        required: true,
+    },
+    postRef: {
+        type: ObjectId,
+        ref: "Post",
+    },
+    reactBy: {
+        type: ObjectId,
+        ref: "User",
+    }
+});
 
 module.exports = mongoose.model("React", reactSchema);
