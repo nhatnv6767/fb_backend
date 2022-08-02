4d855f51be7d1ead8a0952c05686060ac432e2a0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-02 23:21:41 +0900

    #132 React mode, react function 9

--- controllers/react.js
+++ controllers/react.js
@@ -15,6 +15,14 @@ exports.reactPost = async (req, res) => {
                 reactBy: req.user.id,
             });
             await newReact.save();
+        } else {
+            if (check.react == react) {
+                await React.findByIdAndRemove(check._id);
+            } else {
+                await React.findByIdAndUpdate(check._id, {
+                    react: react,
+                });
+            }
         }
     } catch (e) {
         res.status(500).json({message: e.message});
