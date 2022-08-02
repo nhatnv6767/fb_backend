f6081727e673c2677e4cd86333ef2936f07cf1fc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-02 22:55:49 +0900

    #132 React mode, react function 1

--- models/React.js
+++ models/React.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 /* Destructuring the `ObjectId` property from the `mongoose.Schema` object. */
 const {ObjectId} = mongoose.Schema;
 
-const reactSchema = new mongoose.Schema({
-
-});
+const reactSchema = new mongoose.Schema({});
 
 module.exports = mongoose.model("React", reactSchema);
