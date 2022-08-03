e2740c2575c7d114de1650fd10d21b336bf9a6b5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-03 22:57:39 +0900

    #135 Show all reacts 8

--- controllers/react.js
+++ controllers/react.js
@@ -45,8 +45,32 @@ exports.getReacts = async (req, res) => {
             return group;
         }, {});
 
-        console.log(newReacts);
-
+        const finalArray = [
+            {
+                react: 'like',
+                count: newReacts.like ? newReacts.like.length : 0
+            },
+            {
+                react: 'love',
+                count: newReacts.love ? newReacts.love.length : 0
+            },
+            {
+                react: 'haha',
+                count: newReacts.haha ? newReacts.haha.length : 0
+            },
+            {
+                react: 'sad',
+                count: newReacts.sad ? newReacts.sad.length : 0
+            },
+            {
+                react: 'angry',
+                count: newReacts.angry ? newReacts.angry.length : 0
+            },
+            {
+                react: 'wow',
+                count: newReacts.wow ? newReacts.wow.length : 0
+            },
+        ];
         const check = await React.findOne({
             postRef: req.params.id,
             reactBy: req.user.id,
