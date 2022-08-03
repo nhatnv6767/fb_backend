66af88f39da6309144ac14e1850d1607fcc91ea5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-03 22:29:39 +0900

    #135 Show all reacts 1

--- controllers/react.js
+++ controllers/react.js
@@ -38,6 +38,7 @@ exports.getReacts = async (req, res) => {
         * const check1 = reacts.find((x) => x.reactBy.toString() == req.user.id)?.react;
         console.log(check1);
         * */
+        console.log(reacts);
         const check = await React.findOne({
             postRef: req.params.id,
             reactBy: req.user.id,
