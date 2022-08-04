47c8aa1e9a65181ec84204c8846da6d32e8bc292
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-04 23:13:41 +0900

    #139 Display posts for the people you follow and fix issues 13

--- controllers/post.js
+++ controllers/post.js
@@ -15,18 +15,21 @@ exports.getAllPosts = async (req, res) => {
         const following = followingTemp.following;
         const promises = following.map((user) => {
             return Post.find({user: user})
-                .populate("user", "first_name last_name picture username")
+                .populate("user", "first_name last_name picture username cover")
                 .populate("comments.commentBy", "first_name last_name picture username")
                 .sort({createdAt: -1})
                 .limit(10);
         });
         const followingPosts = (await Promise.all(promises)).flat();
         const userPosts = await Post.find({user: req.user.id})
-            .populate("user", "first_name last_name picture username")
+            .populate("user", "first_name last_name picture username cover")
             .populate("comments.commentBy", "first_name last_name picture username")
             .sort({createdAt: -1})
             .limit(10);
         followingPosts.push(...[...userPosts]);
+        followingPosts.sort((a, b) => {
+            return b.createdAt - a.createdAt;
+        });
         res.json(followingPosts);
     } catch (e) {
         res.status(500).json({message: e.message});
