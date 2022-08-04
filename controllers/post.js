08012e2c31148ebd68554c4a205f1efa38ec2572
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-04 22:42:49 +0900

    #139 Display posts for the people you follow and fix issues 4

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,5 @@
 const Post = require("../models/Post");
+const User = require("../models/User");
 exports.createPost = async (req, res) => {
     try {
         const post = await new Post(req.body).save();
@@ -10,11 +11,8 @@ exports.createPost = async (req, res) => {
 
 exports.getAllPosts = async (req, res) => {
     try {
-        const posts = await Post.find().populate("user",
-            "first_name last_name picture username gender"
-            /* Sorting the posts by the date they were created. */
-        ).sort({createdAt: -1});
-        res.json(posts);
+        const following = await User.findById(req.user.id).select("following");
+        console.log(following);
     } catch (e) {
         res.status(500).json({message: e.message});
     }
