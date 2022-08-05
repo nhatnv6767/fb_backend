f784bcc8ab8719cde610fa33617d65d276de64cb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-05 21:10:06 +0900

    #140 Refresh posts when creating post profile + home etc ... 1

--- controllers/post.js
+++ controllers/post.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 exports.createPost = async (req, res) => {
     try {
         const post = await new Post(req.body).save();
+        await post.populate("user", "first_name last_name cover picture username");
         res.json(post);
     } catch (e) {
         res.status(500).json({message: e.message});
