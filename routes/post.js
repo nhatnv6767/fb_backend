9b0ee6a140ebe8c28f2dc71ae8e32e81649a08e3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-06 13:18:17 +0900

    #141 Save, un-save post 1

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,7 @@ const {
     createPost,
     getAllPosts,
     comment,
+    savePost,
 } = require("../controllers/post");
 const {authUser} = require("../middlewares/auth");
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/createPost", authUser, createPost);
 router.get("/getAllPosts", authUser, getAllPosts);
 router.put("/comment", authUser, comment);
+router.put("/savePost/:id", authUser, savePost);
 
 module.exports = router;
